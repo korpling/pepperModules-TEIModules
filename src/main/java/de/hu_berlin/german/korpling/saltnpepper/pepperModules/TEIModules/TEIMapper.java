092844a6c59470479143031f97b96ee67181f4a1
André Röhrig <andre.roehrig@fu-berlin.de>
2014-06-16 13:44:40 +0200

    added TEIMapper

---
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/TEIModules/TEIMapper.java
@@ -0,0 +1,24 @@
+package de.hu_berlin.german.korpling.saltnpepper.pepperModules.TEIModules;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.hu_berlin.german.korpling.saltnpepper.pepper.common.DOCUMENT_STATUS;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.modules.impl.PepperMapperImpl;
+import de.hu_berlin.german.korpling.saltnpepper.salt.SaltFactory;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
+
+public class TEIMapper extends PepperMapperImpl{
+	@Override
+	public DOCUMENT_STATUS mapSDocument() {
+		if(getSDocument()==null){
+			setSDocument(SaltFactory.eINSTANCE.createSDocument());
+		}
+		SDocumentGraph docGraph = SaltFactory.eINSTANCE.createSDocumentGraph();		
+		getSDocument().setSDocumentGraph(docGraph);
+		TEITagLibraryReader reader = new TEITagLibraryReader();
+		this.readXMLResource(reader, getResourceURI());
+		return(DOCUMENT_STATUS.COMPLETED);
+	}
+	private static Logger logger = LoggerFactory.getLogger(TEIMapper.class);
+}
