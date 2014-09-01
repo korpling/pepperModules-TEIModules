0f29c12cb7fc586e94b3768769d87c6873f62e0b
André Röhrig <andre.roehrig@fu-berlin.de>
2014-09-01 17:10:15 +0200

    reader: cleaned up unused else/if-blocks

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/TEIModules/TEITagLibraryReader.java
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/TEIModules/TEITagLibraryReader.java
@@ -689,24 +689,6 @@ public class TEITagLibraryReader extends DefaultHandler2 implements
 				
 			}
 			
-			else if (TAG_ORIGDATE.equals(qName)) {
-			} else if (TAG_MSNAME.equals(qName)) {
-			} else if (TAG_LANGUSAGE.equals(qName)) {
-			} else if (TAG_TEXTLANG.equals(qName)) {
-			} else if (TAG_CHANGE.equals(qName)) {
-			} else if (TAG_SURNAME.equals(qName)) {
-			} else if (TAG_PLACENAME.equals(qName)) {
-			} else if (TAG_MSDESC.equals(qName)) {
-			} 
-			
-			else if (TAG_TITLESTMT.equals(qName))  {
-			}
-			
-			else if (TAG_REVISIONDESC.equals(qName)) {
-			} 
-			
-			else if (TAG_LICENCE.equals(qName)) {
-			} 
 			
 			else if (TAG_TEXT.equals(qName)) {
 				TagStack.push(TAG_TEXT);
@@ -726,48 +708,6 @@ public class TEITagLibraryReader extends DefaultHandler2 implements
 			
 			else if (TAG_BODY.equals(qName)) {
 				//nothing
-			}
-			
-			else if (TAG_FORENAME.equals(qName)) {
-			} else if (TAG_FILEDESC.equals(qName)) {
-			} else if (TAG_LISTBIBL.equals(qName)) {
-			} else if (TAG_COUNTRY.equals(qName)) {
-			} else if (TAG_BIBLSCOPE.equals(qName)) {
-			} else if (TAG_LANGUAGE.equals(qName)) {
-			} else if (TAG_IDNO.equals(qName)) {
-			} else if (TAG_SERIES.equals(qName)) {
-			} else if (TAG_CREATION.equals(qName)) {
-			} else if (TAG_ENCODINGDESC.equals(qName)) {
-			} else if (TAG_COLLECTION.equals(qName)) {
-			} else if (TAG_DATE.equals(qName)) {
-			} else if (TAG_PUBLISHER.equals(qName)) {
-			} else if (TAG_MSIDENTIFIER.equals(qName)) {
-			} else if (TAG_AUTHOR.equals(qName)) {
-			} else if (TAG_AUTHORITY.equals(qName)) {
-			} else if (TAG_MSCONTENTS.equals(qName)) {
-			} else if (TAG_PUBPLACE.equals(qName)) {
-			} else if (TAG_TEI.equals(qName)) {
-			} else if (TAG_REPOSITORY.equals(qName)) {
-			} else if (TAG_MSPART.equals(qName)) {
-			} else if (TAG_SOURCEDESC.equals(qName)) {
-			} else if (TAG_PROFILEDESC.equals(qName)) {
-			} else if (TAG_PUBLICATIONSTMT.equals(qName)) {
-			} else if (TAG_LOCUS.equals(qName)) {
-			} else if (TAG_AVAILABILITY.equals(qName)) {
-			} else if (TAG_ORIGIN.equals(qName)) {
-			} else if (TAG_MSITEM.equals(qName)) {
-			} else if (TAG_INCIPIT.equals(qName)) {
-			} else if (TAG_HISTORY.equals(qName)) {
-			} else if (TAG_REF.equals(qName)) {
-			} else if (TAG_BIBL.equals(qName)) {
-			} else if (TAG_TEIHEADER.equals(qName)) {
-			} 
-			
-			else if (TAG_AB.equals(qName)) {
-			} 
-			else if (TAG_OBJECTTYPE.equals(qName)) {
-			} 
-			else if (TAG_ORIGPLACE.equals(qName)) {
 			}
 		}
 	}
@@ -865,6 +805,11 @@ public class TEITagLibraryReader extends DefaultHandler2 implements
 				}
 			}
 			
+			else if (TAG_TEXT.equals(qName)) {
+				insidetext = false;
+				getSNodeStack().pop();
+			}
+			
 			else if (TAG_SURPLUS.equals(qName)) {
 				if (surplus_removal) {
 					txt.setLength(0);
@@ -901,64 +846,6 @@ public class TEITagLibraryReader extends DefaultHandler2 implements
 			else if (TAG_BODY.equals(qName)) {
 				//nothing
 			}
-			
-			else if (TAG_ORIGDATE.equals(qName)) {
-			} else if (TAG_MSNAME.equals(qName)) {
-			} else if (TAG_LANGUSAGE.equals(qName)) {
-			} else if (TAG_TEXTLANG.equals(qName)) {
-			} else if (TAG_CHANGE.equals(qName)) {
-			} else if (TAG_SURNAME.equals(qName)) {
-			} else if (TAG_PLACENAME.equals(qName)) {
-			} else if (TAG_MSDESC.equals(qName)) {
-			} else if (TAG_TITLESTMT.equals(qName)) {
-			} else if (TAG_REVISIONDESC.equals(qName)) {
-			} else if (TAG_LICENCE.equals(qName)) {
-			} 
-			
-			else if (TAG_TEXT.equals(qName)) {
-				insidetext = false;
-				getSNodeStack().pop();
-			} 
-			
-			else if (TAG_FORENAME.equals(qName)) {
-			} else if (TAG_FILEDESC.equals(qName)) {
-			} else if (TAG_LISTBIBL.equals(qName)) {
-			} else if (TAG_COUNTRY.equals(qName)) {
-			} else if (TAG_BIBLSCOPE.equals(qName)) {
-			} else if (TAG_LANGUAGE.equals(qName)) {
-			} else if (TAG_IDNO.equals(qName)) {
-			} else if (TAG_SERIES.equals(qName)) {
-			} else if (TAG_CREATION.equals(qName)) {
-			} else if (TAG_ENCODINGDESC.equals(qName)) {
-			} else if (TAG_COLLECTION.equals(qName)) {
-			} else if (TAG_DATE.equals(qName)) {
-			} else if (TAG_PUBLISHER.equals(qName)) {
-			} else if (TAG_MSIDENTIFIER.equals(qName)) {
-			} else if (TAG_AUTHOR.equals(qName)) {
-			} else if (TAG_AUTHORITY.equals(qName)) {
-			} else if (TAG_MSCONTENTS.equals(qName)) {
-			} else if (TAG_PUBPLACE.equals(qName)) {
-			} else if (TAG_TEI.equals(qName)) {
-			} else if (TAG_REPOSITORY.equals(qName)) {
-			} else if (TAG_MSPART.equals(qName)) {
-			} else if (TAG_SOURCEDESC.equals(qName)) {
-			} else if (TAG_PROFILEDESC.equals(qName)) {
-			} else if (TAG_PUBLICATIONSTMT.equals(qName)) {
-			} else if (TAG_LOCUS.equals(qName)) {
-			} else if (TAG_AVAILABILITY.equals(qName)) {
-			} else if (TAG_ORIGIN.equals(qName)) {
-			} else if (TAG_MSITEM.equals(qName)) {
-			} else if (TAG_INCIPIT.equals(qName)) {
-			} else if (TAG_HISTORY.equals(qName)) {
-			} else if (TAG_REF.equals(qName)) {
-			} else if (TAG_BIBL.equals(qName)) {
-			} else if (TAG_TEIHEADER.equals(qName)) {
-			} 
-			
-			else if (TAG_AB.equals(qName)) {
-			} else if (TAG_OBJECTTYPE.equals(qName)) {
-			} else if (TAG_ORIGPLACE.equals(qName)) {
-			} 
 		}
 		
 		//remove adequate tag from TagStack
