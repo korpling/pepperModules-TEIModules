b72349ac27edfc8b6c9e9d33d44796c8e770118c
f.zipser@gmx.de <f.zipser@gmx.de>
2015-07-06 15:29:19 +0200

    changed parents pom version and added hoempage and description informations

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/TEIModules/TEIImporter.java
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/TEIModules/TEIImporter.java
@@ -29,57 +29,66 @@ import de.hu_berlin.german.korpling.saltnpepper.pepper.modules.exceptions.Pepper
 import de.hu_berlin.german.korpling.saltnpepper.pepper.modules.impl.PepperImporterImpl;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SElementId;
 
-@Component(name="TEIImporterComponent", factory="PepperImporterComponentFactory")
-public class TEIImporter extends PepperImporterImpl implements PepperImporter{
-	
-	
-	public TEIImporter(){
+@Component(name = "TEIImporterComponent", factory = "PepperImporterComponentFactory")
+public class TEIImporter extends PepperImporterImpl implements PepperImporter {
+
+	public TEIImporter() {
 		super();
 		this.setName("TEIImporter");
+		setSupplierContact(URI.createURI("saltnpepper@lists.hu-berlin.de"));
+		setSupplierHomepage(URI.createURI("https://github.com/korpling/pepperModules-TEIModules"));
+		setDesc("This importer transforms data in TEI format (see http://www.tei-c.org/index.xml) to a Salt model. Please note that this module only supports a sbset of the TEI P5 guidelines. ");
 		this.addSupportedFormat("TEI", "P5 2.6.0", null);
 		this.setProperties(new TEIImporterProperties());
 		this.getSDocumentEndings().add("xml");
 		this.getSDocumentEndings().add("tei");
 	}
-	
-	
-	public PepperMapper createPepperMapper(SElementId sElementId){
+
+	public PepperMapper createPepperMapper(SElementId sElementId) {
 		TEIMapper mapper = new TEIMapper();
-		
+
 		mapper.setResourceURI(getSElementId2ResourceTable().get(sElementId));
-		return(mapper);
-	}
-	
-	@Override
-	public void end() throws PepperModuleException {	
-		super.end();
-	}
-	/**
-	 * <strong>OVERRIDE THIS METHOD FOR CUSTOMIZATION</strong>
-	 * <br/>
-	 * This method is called by the pepper framework and returns if a corpus located at the given {@link URI} is importable
-	 * by this importer. If yes, 1 must be returned, if no 0 must be returned. If it is not quite sure, if the given corpus
-	 * is importable by this importer any value between 0 and 1 can be returned. If this method is not overridden, 
-	 * null is returned.
-	 * @return 1 if corpus is importable, 0 if corpus is not importable, 0 < X < 1, if no definitive answer is possible,  null if method is not overridden 
-	 */
-	public Double isImportable(URI corpusPath){
-		return(null);
+		return (mapper);
 	}
 
-// =================================================== optional ===================================================	
+	@Override
+	public void end() throws PepperModuleException {
+		super.end();
+	}
+
 	/**
-	 * <strong>OVERRIDE THIS METHOD FOR CUSTOMIZATION</strong>
-	 * <br/>
-	 * This method is called by the pepper framework after initializing this object and directly before start processing. 
-	 * Initializing means setting properties {@link PepperModuleProperties}, setting temporary files, resources etc.
-	 * returns false or throws an exception in case of {@link PepperModule} instance is not ready for any reason.
-	 * <br/>
-	 * So if there is anything to do, before your importer can start working, do it here.
-	 * @return false, {@link PepperModule} instance is not ready for any reason, true, else.
+	 * <strong>OVERRIDE THIS METHOD FOR CUSTOMIZATION</strong> <br/>
+	 * This method is called by the pepper framework and returns if a corpus
+	 * located at the given {@link URI} is importable by this importer. If yes,
+	 * 1 must be returned, if no 0 must be returned. If it is not quite sure, if
+	 * the given corpus is importable by this importer any value between 0 and 1
+	 * can be returned. If this method is not overridden, null is returned.
+	 * 
+	 * @return 1 if corpus is importable, 0 if corpus is not importable, 0 < X <
+	 *         1, if no definitive answer is possible, null if method is not
+	 *         overridden
+	 */
+	public Double isImportable(URI corpusPath) {
+		return (null);
+	}
+
+	// =================================================== optional
+	// ===================================================
+	/**
+	 * <strong>OVERRIDE THIS METHOD FOR CUSTOMIZATION</strong> <br/>
+	 * This method is called by the pepper framework after initializing this
+	 * object and directly before start processing. Initializing means setting
+	 * properties {@link PepperModuleProperties}, setting temporary files,
+	 * resources etc. returns false or throws an exception in case of
+	 * {@link PepperModule} instance is not ready for any reason. <br/>
+	 * So if there is anything to do, before your importer can start working, do
+	 * it here.
+	 * 
+	 * @return false, {@link PepperModule} instance is not ready for any reason,
+	 *         true, else.
 	 */
 	@Override
-	public boolean isReadyToStart() throws PepperModuleNotReadyException{
-		return(super.isReadyToStart());
+	public boolean isReadyToStart() throws PepperModuleNotReadyException {
+		return (super.isReadyToStart());
 	}
 }
